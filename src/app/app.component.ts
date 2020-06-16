16eef5094de1ff3aae38ebbb59dc21135134b14b
Akshita13 <akshitakapadia13@gmail.com>
2020-06-16 23:30:07 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  "result"=[
+
+        {
+
+            "employeeId": "08decc11-cf85-4de7-9273-b28d2c2aa115",
+
+            "firstName": "Braun",
+
+            "lastName": "strawman",
+
+            "endDate": null,
+
+            "status": "Green",
+
+            "email": "braun@dispostable.com",
+
+            "phoneNumber": "9228593666",
+
+            "department": ".NET",
+
+            "isActive": true,
+
+            "autoRelease": false,
+
+            "role": "HR",
+
+            "confirmationDate": null
+
+        }
+
+    ]}
+
